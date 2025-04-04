677a1b9a8269db72c222e20e65061a057d74a62d
Furkan Yaman <77271600+ffurkan04@users.noreply.github.com>
2025-04-04 17:40:02 +0300

    Oynanış menüsü tamamladın
    Ana menüdeki oynanışıı (hikaye vs. ya da kontroller gibi şeyleri) anlattığımız yer tamamlandı.

--- Assets/Scripts/MenuScripts/MenuManager.cs
+++ Assets/Scripts/MenuScripts/MenuManager.cs
@@ -3,7 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField]GameObject[] buttons; 
+    [SerializeField]GameObject Text; 
+    bool isOpen;
+
+    void Start()
+    {
+        isOpen=false;
+        Text.SetActive(false);
+    }
     public void GameStart(){
         SceneManager.LoadScene(1);
+    }
+    public void setActiveOfGamePlayMenu(){
+        if(isOpen == false){
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].SetActive(false);
+            }
+            Text.SetActive(true);
+            isOpen=true;
+        }else if(isOpen == true){
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].SetActive(true);
+                Text.SetActive(false);
+                isOpen=false;
+            }
+        }
     }
 }
